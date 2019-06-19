98f8b84d0e778bd0e3c0ba5977085c0ced540865
y8oop <johnglobalplus@gmail.com>
2019-06-19 21:47:25 +0300

    Update herethefile.js

--- herethefile.js
+++ herethefile.js
@@ -1,13 +1,8 @@
 const Discord = require('discord.js'); 
 const client = new Discord.Client(); 
-const examplefile = require('./herethefile.js'); 
+const herethefile = require('./herethefile.js'); 
 
-client.on("message", (message) => {
-  if(message.content.startsWith(PREFIX + " go stream")){
-                client.user.setActivity("my adventure as streamer", {type: "STREAMING"})
-        }
-        
-        });
+
         
         
 client.login(process.env.token);
