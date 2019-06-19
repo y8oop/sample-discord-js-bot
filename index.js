0b73e9f5ee3eaf1c09d88614bd39c144eceadf65
y8oop <johnglobalplus@gmail.com>
2019-06-19 21:41:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js'); 
 const client = new Discord.Client(); 
-const examplefile = require('./herethefile.js'); 
+const herethefile = require('./herethefile.js'); 
 
 //const token = "yourTOKENid"
 
