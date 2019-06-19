8600d39ea545c782c03211b96fa23931c70bee1c
y8oop <johnglobalplus@gmail.com>
2019-06-19 22:54:43 +0300

    Update herethefile.js

--- herethefile.js
+++ herethefile.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 const herethefile = require('./herethefile.js'); 
 
 
+const PREFIX = "s!"
+
 client.on("message", (message) => {
   if(message.content.startsWith(PREFIX + " go watch")){
                 client.user.setActivity("the world", {type: "WATCHING"})
